43742f62429e8596ab428a66de9ce35cd110ad6e
Rupa1806 <ravadarupalatha@gmail.com>
2026-02-01 18:47:31 +0530

    changed function
    The login function has been removed from script.js, eliminating user authentication logic.

--- js /script.js
+++ js /script.js
@@ -197,16 +197,7 @@ function confirmBooking(event) {
     localStorage.removeItem("selectedPackage");
     localStorage.removeItem("packagePrice");
 }
-function login() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
 
-    if (username === "user" && password === "1234") {
-        window.location.href = "hotels.html";
-    } else {
-        alert("Invalid username or password");
-    }
-}
 
 
 
